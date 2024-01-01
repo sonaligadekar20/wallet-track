5f08ebd8dde8fd04fd0ce7f3b6f1bbb08a3ad071
sonaligadekar20 <sonaliygadekar@gmail.com>
2024-01-01 15:42:32 +0530

    added some changes

--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Signup.css';
+import './signup.css';
 import { Link } from "react-router-dom";
 import showToast from "crunchy-toast";
 import Navbar from './../../components/Navbar/Navbar.js';
